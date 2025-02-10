6135b075b68d26197569f60da54ab179a35b0c64
Aiden Romero <aidenromerogarcia@gmail.com>
2025-02-10 10:20:50 +0100

    Update Book.java

--- src/main/java/org/example/classes/Book.java
+++ src/main/java/org/example/classes/Book.java
@@ -2,40 +2,17 @@ package org.example.classes;
 
 public class Book {
     private String title;
-    private String author;
 
-    public Book(String title, String author){
+    public Book (String title){
         this.title = title;
-        this.author = author;
     }
 
-    public String getTitle() {
+    public String getTitle(){
         return title;
-    }
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-    public void setAuthor(String author){
-        this.author = author;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Book book = (Book) obj;
-        return title.equals(book.title) && author.equals(book.author);
     }
 
     @Override
     public String toString() {
-        return "Book {" +
-                "Title =" + title +
-                '\'' +
-                ", Author =" + author + "}";
+        return "Book: " + title;
     }
 }

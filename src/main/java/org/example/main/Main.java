17550a53b4c1696cb4fbbae0a18117b595092553
Aiden Romero <aidenromerogarcia@gmail.com>
2025-02-10 10:19:26 +0100

    Update Main.java

--- src/main/java/org/example/main/Main.java
+++ src/main/java/org/example/main/Main.java
@@ -3,30 +3,10 @@ package org.example.main;
 import org.example.classes.Book;
 import org.example.classes.Library;
 
+import org.example.classes.LibraryManager;
+
 public class Main {
-    public static void main(String[] args) {
-        Library collection = new Library();
-
-        /*Add books
-        collection.addBook(new Book("1984", "George Orwell"));
-        collection.addBook(new Book("The Lord of the Rings", "J.R.R. Tolkien"));
-        collection.addBook(new Book());
-         */
-
-        //Print book list
-        System.out.println("Book List:");
-        for(Book book : collection.getBookList()) {
-            System.out.println(book);
-        }
-
-        /*//Get a book by its position (index)
-        Book book = collection.getBookByPos(2);
-        System.out.println("\nBook at position" + i + ": " + book);
-
-        //Remove a book
-        collection.removeBook(book);
-        System.out.println("");*/
-
-
+    public static void main(String[] args){
+        LibraryManager.run();
     }
 }
